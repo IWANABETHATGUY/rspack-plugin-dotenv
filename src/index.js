b982ad2c38172d69f8276d349ee03494fa0d8cd6
IWANABETHATGUY <iwanabethatguy@qq.com>
2023-03-13 17:37:47 +0800

    feat: 🎸 basic impl

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import dotenv from 'dotenv-defaults'
-import fs from 'fs'
-import { DefinePlugin } from 'webpack'
+import dotenv from "dotenv-defaults";
+import fs from "fs";
+import { DefinePlugin } from "webpack";
 
 // Mostly taken from here: https://github.com/motdotla/dotenv-expand/blob/master/lib/main.js#L4
 const interpolate = (env, vars) => {
-  const matches = env.match(/\$([a-zA-Z0-9_]+)|\${([a-zA-Z0-9_]+)}/g) || []
+  const matches = env.match(/\$([a-zA-Z0-9_]+)|\${([a-zA-Z0-9_]+)}/g) || [];
 
-  matches.forEach((match) => {
-    const key = match.replace(/\$|{|}/g, '')
-    let variable = vars[key] || ''
-    variable = interpolate(variable, vars)
-    env = env.replace(match, variable)
-  })
+  matches.forEach(match => {
+    const key = match.replace(/\$|{|}/g, "");
+    let variable = vars[key] || "";
+    variable = interpolate(variable, vars);
+    env = env.replace(match, variable);
+  });
 
-  return env
-}
+  return env;
+};
 
-const isMainThreadElectron = (target) =>
-  target.startsWith('electron') && target.endsWith('main')
+const isMainThreadElectron = target => target.startsWith("electron") && target.endsWith("main");
 
 class Dotenv {
   /**
@@ -30,122 +29,136 @@ class Dotenv {
    * @param {String} [options.prefix=process.env.] - The prefix, used to denote environment variables.
    * @returns {webpack.DefinePlugin}
    */
-  constructor (config = {}) {
-    this.config = Object.assign({}, {
-      path: './.env',
-      prefix: 'process.env.'
-    }, config)
+  constructor(config = {}) {
+    this.config = Object.assign(
+      {},
+      {
+        path: "./.env",
+        prefix: "process.env.",
+      },
+      config
+    );
   }
 
-  apply (compiler) {
-    const variables = this.gatherVariables()
-    const target = compiler.options.target ?? 'web'
-    const version = (compiler.webpack && compiler.webpack.version) || '4'
+  apply(compiler) {
+    const variables = this.gatherVariables();
+    const target = compiler.options.target ?? "web";
+    const version = (compiler.webpack && compiler.webpack.version) || "4";
     const data = this.formatData({
       variables,
       target,
-      version
-    })
+      version,
+    });
 
-    new DefinePlugin(data).apply(compiler)
+    compiler.hooks.initialize.tap("DotEnvPlugin", () => {
+      let define = compiler.options.builtins.define;
+      compiler.options.builtins.define = {
+        ...define,
+        ...data
+      };
+    });
   }
 
-  gatherVariables () {
-    const { safe, allowEmptyValues } = this.config
-    const vars = this.initializeVars()
+  gatherVariables() {
+    const { safe, allowEmptyValues } = this.config;
+    const vars = this.initializeVars();
 
-    const { env, blueprint } = this.getEnvs()
+    const { env, blueprint } = this.getEnvs();
 
     Object.keys(blueprint).forEach(key => {
-      const value = Object.prototype.hasOwnProperty.call(vars, key) ? vars[key] : env[key]
+      const value = Object.prototype.hasOwnProperty.call(vars, key) ? vars[key] : env[key];
 
-      const isMissing = typeof value === 'undefined' || value === null ||
-        (!allowEmptyValues && value === '')
+      const isMissing = typeof value === "undefined" || value === null || (!allowEmptyValues && value === "");
 
       if (safe && isMissing) {
-        throw new Error(`Missing environment variable: ${key}`)
+        throw new Error(`Missing environment variable: ${key}`);
       } else {
-        vars[key] = value
+        vars[key] = value;
       }
-    })
+    });
 
     // add the leftovers
     if (safe) {
-      Object.keys(env).forEach((key) => {
+      Object.keys(env).forEach(key => {
         if (!Object.prototype.hasOwnProperty.call(vars, key)) {
-          vars[key] = env[key]
+          vars[key] = env[key];
         }
-      })
+      });
     }
 
-    return vars
+    return vars;
   }
 
-  initializeVars () {
-    return (this.config.systemvars) ? Object.assign({}, process.env) : {}
+  initializeVars() {
+    return this.config.systemvars ? Object.assign({}, process.env) : {};
   }
 
-  getEnvs () {
-    const { path, silent, safe } = this.config
+  getEnvs() {
+    const { path, silent, safe } = this.config;
 
-    const env = dotenv.parse(this.loadFile({
-      file: path,
-      silent
-    }), this.getDefaults())
+    const env = dotenv.parse(
+      this.loadFile({
+        file: path,
+        silent,
+      }),
+      this.getDefaults()
+    );
 
-    let blueprint = env
+    let blueprint = env;
     if (safe) {
-      let file = `${path}.example`
+      let file = `${path}.example`;
       if (safe !== true) {
-        file = safe
+        file = safe;
       }
-      blueprint = dotenv.parse(this.loadFile({
-        file,
-        silent
-      }))
+      blueprint = dotenv.parse(
+        this.loadFile({
+          file,
+          silent,
+        })
+      );
     }
 
     return {
       env,
-      blueprint
-    }
+      blueprint,
+    };
   }
 
-  getDefaults () {
-    const { path, silent, defaults } = this.config
+  getDefaults() {
+    const { path, silent, defaults } = this.config;
 
     if (defaults) {
       return this.loadFile({
         file: defaults === true ? `${path}.defaults` : defaults,
-        silent
-      })
+        silent,
+      });
     }
 
-    return ''
+    return "";
   }
 
-  formatData ({ variables = {}, target, version }) {
-    const { expand, prefix } = this.config
+  formatData({ variables = {}, target, version }) {
+    const { expand, prefix } = this.config;
     const formatted = Object.keys(variables).reduce((obj, key) => {
-      const v = variables[key]
-      const vKey = `${prefix}${key}`
-      let vValue
+      const v = variables[key];
+      const vKey = `${prefix}${key}`;
+      let vValue;
       if (expand) {
-        if (v.substring(0, 2) === '\\$') {
-          vValue = v.substring(1)
-        } else if (v.indexOf('\\$') > 0) {
-          vValue = v.replace(/\\\$/g, '$')
+        if (v.substring(0, 2) === "\\$") {
+          vValue = v.substring(1);
+        } else if (v.indexOf("\\$") > 0) {
+          vValue = v.replace(/\\\$/g, "$");
         } else {
-          vValue = interpolate(v, variables)
+          vValue = interpolate(v, variables);
         }
       } else {
-        vValue = v
+        vValue = v;
       }
 
-      obj[vKey] = JSON.stringify(vValue)
+      obj[vKey] = JSON.stringify(vValue);
 
-      return obj
-    }, {})
+      return obj;
+    }, {});
 
     // We have to stub any remaining `process.env`s due to Webpack 5 not polyfilling it anymore
     // https://github.com/mrsteele/dotenv-webpack/issues/240#issuecomment-710231534
@@ -153,33 +166,31 @@ class Dotenv {
     // https://webpack.js.org/configuration/target
     if (this.shouldStub({ target, version })) {
       // Results in `"MISSING_ENV_VAR".NAME` which is valid JS
-      formatted['process.env'] = '"MISSING_ENV_VAR"'
+      formatted["process.env"] = '"MISSING_ENV_VAR"';
     }
 
-    return formatted
+    return formatted;
   }
 
-  shouldStub ({ target: targetInput, version }) {
-    if (!version.startsWith('5')) {
-      return false
+  shouldStub({ target: targetInput, version }) {
+    if (!version.startsWith("5")) {
+      return false;
     }
 
-    const targets = Array.isArray(targetInput) ? targetInput : [targetInput]
+    const targets = Array.isArray(targetInput) ? targetInput : [targetInput];
 
     return targets.every(
       target =>
         // If configured prefix is 'process.env'
-        this.config.prefix === 'process.env.' &&
+        this.config.prefix === "process.env." &&
         // If we're not configured to never stub
         this.config.ignoreStub !== true &&
         // And
-        (
-          // We are configured to always stub
-          this.config.ignoreStub === false ||
+        // We are configured to always stub
+        (this.config.ignoreStub === false ||
           // Or if we should according to the target
-          (!target.includes('node') && !isMainThreadElectron(target))
-        )
-    )
+          (!target.includes("node") && !isMainThreadElectron(target)))
+    );
   }
 
   /**
@@ -188,12 +199,12 @@ class Dotenv {
    * @param {Boolean} config.silent - If true, suppress warnings, if false, display warnings.
    * @returns {Object}
    */
-  loadFile ({ file, silent }) {
+  loadFile({ file, silent }) {
     try {
-      return fs.readFileSync(file, 'utf8')
+      return fs.readFileSync(file, "utf8");
     } catch (err) {
-      this.warn(`Failed to load ${file}.`, silent)
-      return {}
+      this.warn(`Failed to load ${file}.`, silent);
+      return {};
     }
   }
 
@@ -202,9 +213,9 @@ class Dotenv {
    * @param {String} msg - The message.
    * @param {Boolean} silent - If true, display the message, if false, suppress the message.
    */
-  warn (msg, silent) {
-    !silent && console.warn(msg)
+  warn(msg, silent) {
+    !silent && console.warn(msg);
   }
 }
 
-export default Dotenv
+export default Dotenv;
